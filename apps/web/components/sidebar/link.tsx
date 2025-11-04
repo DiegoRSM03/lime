1b3db76fe4ef7e91f7905f0fff1d7b01765fdf8e
Diego Sanchez <diegorsm03@gmail.com>
2025-11-04 13:24:15 -0300

    fix: better colors choice for links background

--- apps/web/components/sidebar/link.tsx
+++ apps/web/components/sidebar/link.tsx
@@ -11,7 +11,7 @@ const SidebarLink = ({ href, label, target }: SidebarLinkProps) => {
     <Link
       href={href}
       target={target}
-      className="flex items-center gap-2 h-10 px-3 rounded-lg bg-lime-950 hover:bg-lime-900 transition-colors duration-200"
+      className="flex items-center gap-2 h-10 px-3 rounded-lg bg-blue-950/30 hover:bg-blue-950/60 transition-colors duration-200"
     >
       <span className="text-base font-medium">{label}</span>
     </Link>
