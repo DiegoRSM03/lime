0e87ee7cf1b7a15322c04244e39cd49e2e57b71c
Diego Sanchez <diegorsm03@gmail.com>
2025-11-05 10:08:29 -0300

    fix: solve swagger ui not reaching production API url

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3001',
+      'http://localhost:3000',
       'https://*.vercel.app',
       process.env.FRONTEND_URL,
     ],
